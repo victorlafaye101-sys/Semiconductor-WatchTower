e210771f36280ba912baf9a3f9e0ceac926b1858
HelenFeng <victorlafaye101@gmail.com>
2026-06-11 19:45:14 +0800

    fix: Railway production start (tsx in dependencies)

--- server/index.ts
+++ server/index.ts
@@ -341,6 +341,6 @@ app.use((_req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
